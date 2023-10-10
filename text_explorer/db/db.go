a52b60bcf188efb2c2ca9fce4cb76ffb6a68bda2
yurcik45 <yurcik45mail@gmail.com>
2023-10-10 21:45:45 +0300

    add select one, select all, delete article from bd

--- text_explorer/db/db.go
+++ text_explorer/db/db.go
@@ -35,14 +35,32 @@ func DB_connect() error {
 	return nil
 }
 
-func Article_add(title string, text string, user_id int8) error {
+func Article_get() error {
+	sqlStatement := "SELECT * FROM articles;"
+	_, err := db_connection.Exec(sqlStatement)
+	return err
+}
+
+func Article_get_all(id uint8) error {
+	sqlStatement := "SELECT * FROM articles WHERE id = $1;"
+	_, err := db_connection.Exec(sqlStatement, id)
+	return err
+}
+
+func Article_add(title string, text string, user_id uint8) error {
 	sqlStatement := "INSERT INTO articles (title, text, user_id) VALUES ($1, $2, $3)"
 	_, err := db_connection.Exec(sqlStatement, title, text, user_id)
 	return err
 }
 
-func Article_update(title string, text string, id int8) error {
+func Article_update(title string, text string, id uint8) error {
 	sqlStatement := "UPDATE articles SET title = $1, text = $2 WHERE id = $3;"
 	_, err := db_connection.Exec(sqlStatement, title, text, id)
+	return err
+}
+
+func Article_delete(id uint8) error {
+	sqlStatement := "DELETE FROM articles WHERE id = $1;"
+	_, err := db_connection.Exec(sqlStatement, id)
 	return err
 }
--- text_explorer/schema.sql
+++ text_explorer/schema.sql
@@ -7,9 +7,15 @@ CREATE TABLE articles (
    user_id INT
 );
 
+SELECT * FROM articles;
+
+SELECT * FROM articles WHERE id = 1;
+
 INSERT INTO articles (title, text, user_id)
 VALUES ('test title', 'test text', 045);
 
 UPDATE articles
 SET title='title updated', text='text updated'
 WHERE id = 1;
+
+DELETE FROM articles WHERE id = 1;
